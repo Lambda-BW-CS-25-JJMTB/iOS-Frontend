e316ed49d73af354b9d8181d22bc027dd0ed7bae
Michael Redig <mredig@gmail.com>
2020-02-02 16:18:50 -0600

    websocket test

---
+++ websocket test/websocket test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  websocket test
+//
+//  Created by Michael Redig on 2/2/20.
+//  Copyright © 2020 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
